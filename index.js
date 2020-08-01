7ad4b80688094d40df67c7700358bb846b1198b3
abhijithb-juego <68430190+abhijithb-juego@users.noreply.github.com>
2020-08-01 14:47:56 +0530

    update data

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 exports.getData = function () {
-  return "[{'id':'1','title':'PHP'},{'id':'2','title':'NODE'}]";
+  return "[{'id':'1','title':'PHP'},{'id':'2','title':'NODE'},{'id':'3','title':'REACT'}]";
 }
